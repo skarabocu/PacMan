e76c0863d622826a20418dd2e89e04e665ffb93a
Sinan Karabocuoglu <45086880+skarabocu@users.noreply.github.com>
2018-11-23 15:12:03 -0500

    Add files via upload

---
+++ PacMan.java
@@ -0,0 +1,10 @@
+//Kaan Cinar && Bogachan Arslan && Onder Soydal && Sinan Karabocuoglu
+//PacMan Tester
+//24.04.2018
+public class PacMan
+{
+  public static void main(String [] args)
+  {
+    PacFrame k = new PacFrame(); //initializes the frame
+  }
+}
